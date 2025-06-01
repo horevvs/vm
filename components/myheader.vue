80bdbfd5ea27c37f8ea328956e1ac365048d6903
Horev Vladimir <vshorev@yandex.ru>
2025-06-01 16:02:27 +0700

    уу

--- components/myheader.vue
+++ components/myheader.vue
@@ -3,14 +3,19 @@
         <div class="p-2 flex"><img src="../public/images/Vm_logo.png" alt="">
             <p class="mt-5 text-3xl head">VM-IT</p>
         </div>
-        <div class="flex  justify-around w-1/3 mt-5 "><a href="">22</a><a href="">22</a><a href="">22</a></div>
-        <div class="mt-5">5</div>
+        <div class="flex justify-around w-1/3 mt-8">
+            <a href="" class="text-xl head">Услуги</a>
+            <a href="" class="text-xl head">Проекты</a>
+            <a href="" class="text-xl head">Статьи</a>
+        </div>
+        <!-- <div class="text-xl head  p-3 border-4 rounded-xl"> Контакты</div> -->
+       <a href="" class="text-xl head my-5 px-5  p-3 border-4 rounded-3xl">Контакты</a>
     </div>
 </template>
 
 <style>
-.head{
-  font-family:Montserrat, sans-serif;
-  font-weight: 700;
+.head {
+    font-family: Montserrat, sans-serif;
+    font-weight: 700;
 }
 </style>
---
+++ pages/articles.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="text-5xl">
+     Главная1
+ 
+    </div>
+  </template>
