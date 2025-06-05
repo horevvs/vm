d6ac8b20105d0b60ae16deda49b302cb788226d7
Horev Vladimir <95493697+horevvs@users.noreply.github.com>
2025-06-05 18:45:25 +0700

    1

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,13 @@
 <template>
-
   <div class="">
     <div class=" h-72 font-medium">
       <H1 class="text-center text-5xl font-black ">IT-компания полного цикла:</H1>
       <H2 class="text-center text-5xl font-black leading-relaxed">разработка, дизайн и продвижение</H2>
       <div class="text-center flex justify-center mt-5 ">
         <H2 class="text-center text-xl "> Мы разрабатываем сайты и web-приложения, помогаем оцифровать ваши
-          бизнес-процессы. <br>
-          Мы работаем начиная с концепции вашего приложения до полного запуска и продвижения вашего продукта
+          бизнес-процессы.
+          <br>
+          Мы работаем начиная с концепции вашего приложения до полного запуска и продвижения вашего продукта.
         </H2>
       </div>
     </div>
@@ -23,10 +23,10 @@
         <div class="container">
           <Carousel class="my-12 px-12" :transition="1000" v-bind="carouselConfig">
             <Slide class="px-1" v-for="slide in store.images" :key="slide">
-              <div class="h-72 w-72 rounded-3xl  bg-sky-200 shadow-xl">
+              <div class="h-68 w-72 rounded-3xl  bg-sky-100 shadow-xl">
                 <img :src="slide.img" class="">
-                <p class="mx-2 text-xl mt-2 font-bold ">{{ slide.type }}</p>
-                <p class="mx-2 text-lg font-normal">{{ slide.long }}</p>
+                <p class="mx-5 text-lg mt-2 font-bold ">{{ slide.type }} </p>
+                <p class="mx-5 text-lg font-normal">{{ slide.long }}</p>
               </div>
             </Slide>
             <template #addons>
@@ -36,19 +36,64 @@
         </div>
       </div>
     </div>
+
     <hr class=" border-blue-300 border-1" />
-    <div class=" mt-2 ">
-      <div class="flex  container justify-center ">
-        <div class="border-2  container  h-72">
-          <div class="border-2 flex">
-            <div>1</div>
-            <div>2</div>
+
+    <div class=" my-12  ">
+      <div class="text-xl flex justify-center font-black">
+        <div class="container mt-2">Наши преимущества</div>
+      </div>
+      <div class="flex justify-center">
+        <div class=" container  flex mt-5">
+          <div class="border-blue-300 border-t-8 border-b-8 border-l-8 w-2/3">
+            <div class="flex h-42">
+              <div
+                class=" p-5 w-3/5 border-b-4  border-r-4 border-blue-300 transition-all duration-300 hover:bg-blue-300 hover:text-slate-50">
+                <p class="text-2xl font-bold">Надежность</p>
+                <p class="text-lg">Мы строим долгосрочное отношения с нашими клиентами, соблюдаем сроки, гарантируем
+                  качество работы</p>
+              </div>
+              <div
+                class="p-5 w-2/5 border-b-4 border-l-4 border-blue-300 group p-5 w-3/5 border-b-4 border-r-4 border-blue-300 transition-all duration-300 hover:bg-blue-300 hover:text-slate-50">
+                <p class="text-2xl font-bold">Ответственность</p>
+                <p class="text-lg">Мы несем ответственость за результаты нашей работы, стремимся к устойчивому развитию
+                </p>
+              </div>
+
+            </div>
+            <div class="flex h-42 ">
+              <div
+                class="p-5 w-1/2 border-1 border-r-4 border-t-4 border-blue-300 transition-all duration-300 hover:bg-blue-300 hover:text-slate-50">
+                <p class="text-2xl  font-bold">Прозрачность</p>
+                <p class="text-lg">Открытость в общении в отношении к Вам.
+                </p>
+              </div>
+              <div
+                class="p-5 w-1/2 border-l-4  border-r-4 border-t-4 border-blue-300 transition-all duration-300 hover:bg-blue-300 hover:text-slate-50">
+                <p class="text-2xl   font-bold">Индивидуальных подход</p>
+                <p class="text-lg">Принимаем Ваши потребности и создаем решения. которые максимольно соответствуют
+                  ожиданиям.
+                </p>
+              </div>
+            </div>
           </div>
-          <div class="border-2"> 2</div>
+          <div
+            class=" p-5 border-8 border-blue-300 w-1/3 transition-all duration-300 hover:bg-blue-300 hover:text-slate-50">
+            <p class="text-2xl font-bold">Весь спектр услуг</p>
+            <p class="text-lg"> Мы предлагаем все необходимые решения для вашего бизнеса в одной компании. от разработки
+              сайта до сопровождения после внедрения продукта </p>
+          </div>
         </div>
-        <div class="border-2">весь спектр услуг</div>
       </div>
     </div>
+    <hr class=" border-blue-300 border-1 mt-12" />
+    <div class=" container flex justify-center  mx-auto mt-12">
+      <div class="text-xl mt-2 font-black  w-1/2">Работаем со всеми</div>
+      <div class=" mt-2  w-1/2">Мы специализируемся на работе с цифровыми продуктами и брендами, независимо от размера и
+        стадии жизненного цикла проекта, от стартапов до устоявшихся компаний, стремящихся создать бренд. которые будет
+        выделяться</div>
+    </div>
+    <div class="text-xl mt-2 font-black border-4 container mx-auto">Работаем со всеми2</div>
   </div>
 </template>
 
@@ -61,7 +106,7 @@ import 'vue3-carousel/carousel.css'
 import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
 
 const carouselConfig = {
-  itemsToShow: 4,
+  itemsToShow: 4.5,
   wrapAround: true,
 }
 
